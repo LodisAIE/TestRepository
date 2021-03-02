7ad966ad8e8839c83ab623d92169cee161aada1d
Lodis Perkins <lodisperkins44@gmail.com>
2021-03-01 19:13:56 -0600

    Added movement script for Van

---
+++ Assets/Scripts/Movement/MovementBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += _velocity * Time.deltaTime;
+    }
+}
